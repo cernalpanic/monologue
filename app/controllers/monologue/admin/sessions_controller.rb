f9121c29c1ea40606e76f548273df7c9f0874475
Todd Holdman <todbot@gmail.com>
2014-12-26 18:11:45 -0600

    Update sessions_controller.rb

--- app/controllers/monologue/admin/sessions_controller.rb
+++ app/controllers/monologue/admin/sessions_controller.rb
@@ -1,12 +1,14 @@
 class Monologue::Admin::SessionsController < Monologue::Admin::BaseController
   skip_before_filter :authenticate_user!
-  
   def new
+    if current_user
+      session[:monologue_user_id] = current_user.id
+      redirect_to admin_url, notice: t("monologue.admin.sessions.messages.logged_in")
+    end
   end
-
   def create
-    user = Monologue::User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+    user = User.find_by_email(params[:email])
+    if user.valid_password?(params[:password])
       session[:monologue_user_id] = user.id
       redirect_to admin_url, notice: t("monologue.admin.sessions.messages.logged_in")
     else
@@ -14,8 +16,9 @@ class Monologue::Admin::SessionsController < Monologue::Admin::BaseController
       render "new"
     end
   end
-
   def destroy
+    #devise
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
     session[:monologue_user_id] = nil
     redirect_to admin_url, notice: t("monologue.admin.sessions.messages.logged_out")
   end
